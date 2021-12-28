592aba31e3f550781e9620b10d8010972b2ba35a
Rakshit-Varshney <75945445+Rakshit-Varshney@users.noreply.github.com>
2021-12-28 15:27:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas=createCanvas(350,350);
+    canvas.center();
+    background("white");
+
+}
+function clearCanvas(){
+    background("white");
+}
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet');
+}
